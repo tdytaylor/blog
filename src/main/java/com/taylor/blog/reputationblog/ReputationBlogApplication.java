c1e4e097997ab49270e819ff8ac22a7c8db41863
tdytaylor <949722633@qq.com>
2018-09-24 00:00:32 +0800

    init project

---
+++ src/main/java/com/taylor/blog/reputationblog/ReputationBlogApplication.java
@@ -0,0 +1,12 @@
+package com.taylor.blog.reputationblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReputationBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReputationBlogApplication.class, args);
+	}
+}
